2f11f2f1944bcf14aec9b4d15f5407aa89f7d884
Gregory Dodd <gdodd97@gmail.com>
2023-05-15 21:18:30 -0400

    Built random logic into getComputerChoice function in rps.js

--- rps.js
+++ rps.js
@@ -1,6 +1,10 @@
+//computer generated choice
 function getComputerChoice(){
     const comp_choice = ["Rock","Paper","Scissors"];
 
     let size = comp_choice.length;
-    return comp_choice
+    return comp_choice[Math.floor(Math.random() * size)]
 }
+console.log(getComputerChoice())
+
+
